da7fa4edc4b5056812f9acf7fae07a8f90b63880
julio hernandez <julioandres72@gmail.com>
2020-10-02 15:39:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -42,6 +42,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[row][key] = aoh[row][new_value]
+  aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
